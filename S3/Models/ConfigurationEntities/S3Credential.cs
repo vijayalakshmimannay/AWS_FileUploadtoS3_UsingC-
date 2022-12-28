0a65231b8737a66e8d71f3d016dff3f1d253996d
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-12-28 15:39:37 +0530

    [vijayalakshmi] Read AWS Secret and access Key from Environment System Variables

---
+++ S3/Models/ConfigurationEntities/S3Credential.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S3.Models.ConfigurationEntities
+{
+    public class S3Credential
+    {
+        public string AccessKey { get; set; } = null;
+        public string SecretKey { get; set; } = null;
+        public string BucketName { get; set; } = null;
+
+    }
+}
---
+++ S3/Models/DTO/S3DTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S3.Models.DTO
+{
+    public class S3DTO
+    {
+        public string Message { get; set; }
+        public string StatusCode { get; set; }
+    }
+}
